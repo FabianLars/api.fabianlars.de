1c5f2538f79c68028ba8e540c7236a02a7acb9c1
FabianLars <fabianlars@fabianlars.de>
2021-08-03 10:42:49 +0200

    fix /champions return type.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, net::SocketAddr};
+use std::{collections::HashMap, error::Error, net::SocketAddr};
 
 use axum::{
     extract::{Extension, UrlParams},
@@ -9,10 +9,26 @@ use axum::{
 };
 use chrono::{DateTime, NaiveDate, Utc};
 use hyper::StatusCode;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, PgPool};
-use tokio::fs::read_to_string;
+use tokio::fs::read;
 use version_compare::{CompOp, VersionCompare};
+
+#[derive(Deserialize, Serialize)]
+struct Champ {
+    name: String,
+    codename: String,
+    alias: String,
+    id: i32,
+    skins: Vec<Skin>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Skin {
+    id: i32,
+    id_long: i32,
+    name: String,
+}
 
 #[derive(Serialize)]
 struct Rotation {
@@ -75,22 +91,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn champions() -> Result<impl IntoResponse, StatusCode> {
-    let file = read_to_string("./champions.json")
+    let file = read("./champions.json")
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    // We could return the file straight from the filesystem, but let's check its validity first
+    let json: HashMap<String, Champ> =
+        serde_json::from_slice(&file).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    Ok(response::Json(file))
+    Ok(response::Json(json))
 }
 
 async fn champion(UrlParams((id,)): UrlParams<(u16,)>) -> Result<impl IntoResponse, StatusCode> {
-    let file = read_to_string("./champions.json")
+    let file = read("./champions.json")
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let json: serde_json::Value =
-        serde_json::from_str(&file).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let mut json: HashMap<String, Champ> =
+        serde_json::from_slice(&file).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    match json.get(id.to_string()) {
-        Some(champ) => Ok(champ.to_string()),
+    match json.remove(&id.to_string()) {
+        Some(champ) => Ok(response::Json(champ)),
         None => Err(StatusCode::NOT_FOUND),
     }
 }
